94b680458c6b8c08d2adf83e9adc1cf9c5d008f1
Austin Fischer <alfischer33@gmail.com>
2021-03-24 09:46:16 -0600

    heroku commit

--- travelnetwork/settings.py
+++ travelnetwork/settings.py
@@ -20,7 +20,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = str.(os.getenv('SECRET_KEY'))
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
